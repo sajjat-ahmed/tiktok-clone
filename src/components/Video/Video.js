478313627b4890c30512ad8699f233b7f4dabd2a
Sajjat Ahmed <67552370+sajjat-ahmed@users.noreply.github.com>
2020-11-14 09:15:16 +0600

    Update Video.js

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import VideoFooter from '../VideoFooter/VideoFooter';
 import VideoSidebar from '../VideoSidebar/VideoSidebar';
 import './Video.css';
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
+
+    const [playing, setPlaying] = useState(false);
+    const videoRef = useRef();
+    
+    const onVideoPress = () => {
+        if(playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+    }
+
+
     return (
         <div className="video">
+            <video
+                className="video__player"
+                loop
+                onClick={onVideoPress}
+                ref={videoRef}
+                src={url}
+            ></video>
+
             <VideoFooter
-                channel="refehqazi"
-                description="Check out this dance"
-                song="Usher - Yeah"
+                channel={channel}
+                description={description}
+                song={song}
             />
+
             <VideoSidebar
-                likes={122}
-                messages={76}
-                shares={44}
+                likes={likes}
+                messages={messages}
+                shares={shares}
             />
         </div>
     )
