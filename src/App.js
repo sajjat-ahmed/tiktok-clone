f597337de10cd42806a19d10613cd4581423e149
Sajjat Ahmed <67552370+sajjat-ahmed@users.noreply.github.com>
2020-11-14 09:14:45 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,42 @@ function App() {
     <div className="app">
       😀 😃 😄 😁 😆 😅 🤣 😂 🙂 🙃 😉 😊 😇 🥰 😍 🤩 😘 😗 😚 😙 😋 😛 😜 🤪 😝 🤑 🤗 🤭 🤫 🤔 🤐 🤨 😐 😑 😶 😏 😒 🙄 😬 🤥 😌 😔 😪 🤤 😴 😷 🤒 🤕 🤢 🤮 🤧 🥵 🥶 🥴 😵 🤯 🤠 🥳 😎 🤓 🧐 😕 😟 🙁 😮 😯 😲 😳 🥺 😦 😧 😨 😰 😥 😢 😭 😱 😖 😣 😞 😓 😩 😫 😤 😡 😠 🤬 😈 👿
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          url=""
+          channel=""
+          song=""
+          likes={222}
+          messages={55}
+          description=""
+          shares={34}
+        />
+        <Video
+          url=""
+          channel=""
+          song=""
+          likes={55}
+          messages={45}
+          description=""
+          shares={11}
+        />
+        <Video
+          url=""
+          channel=""
+          song=""
+          likes={555}
+          messages={67}
+          description=""
+          shares={89}
+        />
+        <Video
+          url=""
+          channel=""
+          song=""
+          likes={90}
+          messages={77}
+          description=""
+          shares={23}
+        />
       </div>
     </div>
   );
